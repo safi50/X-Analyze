c8fe83734bedf5407eef62220466ebee0859c8ce
safi50 <safi.shah01@outlook.com>
2023-12-30 02:02:55 +0500

    X-Analyze Project

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify, render_template
+from lda import SignIn, get_tweets,get_user_details, get_topics, gen_wordclouds
+from dotenv import load_dotenv
+
+load_dotenv() # Load environment variables
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', wordcloud_path=None)  # Pass None initially
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    username = request.form['username']
+    twitter_app = SignIn()  # Avoid using the same name 'app' as it shadows the Flask 'app'
+    user = get_user_details(twitter_app, username)  # Retrieve user details
+
+    if user is None:
+        error_message = "The User Account wasn't Found"
+        return render_template('index.html', error_message=error_message)
+
+    tweets = get_tweets(twitter_app, username)
+    lda_model = get_topics(tweets, 1)
+    gen_wordclouds(lda_model, 1)
+    
+    wordcloud_path = 'static/wordcloud_topic_1.png'
+    return render_template('index.html', wordcloud_path=wordcloud_path, user_details=user)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ lda.py
@@ -0,0 +1,98 @@
+import re
+import os
+from dotenv import load_dotenv
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
+from tweety import Twitter
+from tweety.exceptions_ import UserNotFound
+from gensim import corpora
+from gensim.models.ldamodel import LdaModel
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+
+# Ensure NLTK data is downloaded
+# nltk.download('stopwords')
+# nltk.download('wordnet')
+
+# def SignIn():
+#     app = Twitter("session")
+#     app.sign_in("x_analyze", "YY3nSOYJIzVRZy9A5b5o")
+#     return app
+
+load_dotenv() # Load environment variables
+
+def SignIn():
+    twitter_username = os.environ.get('TWITTER_USERNAME')
+    twitter_password = os.environ.get('TWITTER_PASSWORD')
+    print(twitter_username, twitter_password)
+    if not twitter_username or not twitter_password:
+        raise ValueError("Twitter credentials are not set in the environment variables.")
+    app = Twitter("session")
+    app.sign_in(twitter_username, twitter_password)
+    return app
+
+    
+def get_tweets(app, username):
+    try: 
+        all_tweets = app.get_tweets(username)
+        tweets = []
+        for tweet in all_tweets:
+            if hasattr(tweet, 'tweet_body') and tweet.tweet_body:
+                tweets.append(tweet.tweet_body)
+        return tweets
+    except UserNotFound:
+        return None
+
+
+def get_user_details(app, username):
+    try:
+        user = app.get_user_info(username)
+        return user
+    except UserNotFound:
+        return None
+
+
+def processTweet(tweet):
+    tokenizer = RegexpTokenizer(r'\w+')
+    en_stopwords = set(stopwords.words('english'))
+    en_stopwords.update(['rt', 'via'])  
+    tweet = tweet.lower()
+
+    # Remove URLs
+    tweet = re.sub(r'http\S+|www\S+|https\S+', '', tweet, flags=re.MULTILINE)
+
+    # Remove user @ mentions and hashtags
+    tweet = re.sub(r'@\w+|#\w+', '', tweet)
+
+    # Remove non-ASCII characters
+    tweet = re.sub(r'[^\x00-\x7F]+',' ', tweet)
+
+    # Tokenize the tweet
+    tokens = tokenizer.tokenize(tweet)
+
+    # Remove stopwords and short words
+    new_tokens = [token for token in tokens if token not in en_stopwords and len(token) > 2]
+
+    return new_tokens
+
+
+def get_topics(tweets, num_topics=2):
+    processed_tweets = [processTweet(tweet) for tweet in tweets]
+    dictionary = corpora.Dictionary(processed_tweets)
+    doc_term_matrix = [dictionary.doc2bow(tweet) for tweet in processed_tweets]
+    lda_model = LdaModel(doc_term_matrix, num_topics=num_topics, id2word=dictionary, passes=100)
+    return lda_model
+
+def gen_wordclouds(lda_model, num_topics):
+    for t in range(num_topics):
+        topic_terms = dict(lda_model.show_topic(t, 180))
+        wordcloud = WordCloud(width=1200, height=500, background_color='white')
+        wordcloud.generate_from_frequencies(topic_terms)
+        wordcloud.to_file(f'static/wordcloud_topic_{t+1}.png')  # Save as static file
+
+
+
+if __name__ == '__main__':
+    SignIn()
